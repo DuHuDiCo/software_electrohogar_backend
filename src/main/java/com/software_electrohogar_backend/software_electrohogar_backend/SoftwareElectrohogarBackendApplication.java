a984a1dd9d787a4417a9b31d52c40f06efecf56b
Duvan Diaz <dudicodiaz@gmail.com>
2022-09-09 17:05:44 -0500

    commit initial

---
+++ src/main/java/com/software_electrohogar_backend/software_electrohogar_backend/SoftwareElectrohogarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.software_electrohogar_backend.software_electrohogar_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftwareElectrohogarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SoftwareElectrohogarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/software_electrohogar_backend/software_electrohogar_backend/SoftwareElectrohogarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.software_electrohogar_backend.software_electrohogar_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftwareElectrohogarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
